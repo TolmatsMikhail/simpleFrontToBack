0abbc64caf43f18cdbb1c5a0c12a629f1f6d4fb9
Михаил Толмац <m.tolmaz@isimplelab.com>
2017-10-13 18:05:50 +0300

    simpleFrontToBack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  people: any[] = [
+  	{
+  		name: 'First name',
+  		age: '18'
+  	},
+  	{
+  		name: 'Second name',
+  		age: '26'
+  	},
+  	{
+  		name: 'Third name',
+  		age: '38'
+  	}
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  	console.log('I\'m working! ');
+  }
+
+  clickOnButton() {
+  	;
+  }
 }
